61c750399c30ebd6ed302150fe1ae22ed37b92d3
Elclark <mail@elclark.my.id>
2022-06-19 19:58:38 +0800

    :rotating_light: test: add more tests

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,21 @@
 import { expect, test } from '@playwright/test'
 
-test('index page has expected h1', async ({ page }) => {
+test('Checking Index H1', async ({ page }) => {
 	await page.goto('/')
 	expect(await page.textContent('h1')).toBe("I'm Elclark, Student and software engineer.")
+})
+
+test('Checking About', async ({ page }) => {
+	await page.goto('/about')
+	expect(await page.textContent('h1')).toBe('About Me')
+})
+
+test('Checking Contact', async ({ page }) => {
+	await page.goto('/contact')
+	expect(await page.textContent('h1')).toBe('Contact Me')
+})
+
+test('Checking Blog', async ({ page }) => {
+	await page.goto('/blog/welcome')
+	expect(await page.textContent('h1')).toBe('Welcome to Elclark Blog!')
 })
