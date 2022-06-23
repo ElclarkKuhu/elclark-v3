e98f5f26e2d692c1769bdc939c46643fca3a364c
Elclark <mail@elclark.my.id>
2022-06-24 05:18:31 +0800

    :bug: fix: return 404 when blog not found

--- src/routes/blog/[slug].js
+++ src/routes/blog/[slug].js
@@ -1,9 +1,17 @@
 import data from '$data/blog.json'
 
 export async function get({ params }) {
+	const blog = data.find((post) => post.slug === params.slug)
+
+	if (!blog) {
+		return {
+			status: 404
+		}
+	}
+
 	return {
 		body: {
-			blog: data.find((post) => post.slug === params.slug),
+			blog,
 			more: data.filter((post) => post.slug !== params.slug).slice(0, 5)
 		}
 	}
