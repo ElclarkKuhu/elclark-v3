7649cecc6f9efa1bee602e2b4d527e1860b45474
Elclark <mail@elclark.my.id>
2022-07-14 19:30:10 +0800

    :recycle: makesure metadata prerendered?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,8 @@
 <script>
-	import { page } from '$app/stores'
 	import formatDate from '$lib/formatDate'
 
 	import Card from '$components/blog/card.svelte'
 	import Hero from '$components/hero.svelte'
-
-	const title = "Hi, I'm Elclark!"
-	const description =
-		"I'm a software engineer and system administrator from Manado, Indonesia. I also a university student and UI/UX designer. I'm passionate about technology, science, music, and gaming."
-	const image = 'https://elclark.my.id/img/brand/elclark.png'
 
 	export let blogs
 
@@ -31,15 +25,18 @@
 
 <svelte:head>
 	<!-- Metadata -->
-	<title>{title}</title>
-	<link rel="canonical" href={$page.url.href} />
+	<title>"Hi, I'm Elclark!"</title>
+	<link rel="canonical" href="https://elclark.my.id/" />
 
 	<!-- Basic HTML Meta Tags -->
 	<meta
 		name="keywords"
 		content="Elclark, Code, software engineer, technology, programming, science, music, gaming, "
 	/>
-	<meta name="description" content={description} />
+	<meta
+		name="description"
+		content="I'm a software engineer and system administrator from Manado, Indonesia. I also a university student and UI/UX designer. I'm passionate about technology, science, music, and gaming."
+	/>
 	<meta name="subject" content="Elclark Personal Website" />
 	<meta name="copyright" content="Elclark" />
 	<meta name="language" content="EN" />
@@ -48,29 +45,35 @@
 	<meta name="author" content="Elclark, founder@elclark.my.id" />
 	<meta name="designer" content="Elclark, founder@elclark.my.id" />
 	<meta name="owner" content="Elclark" />
-	<meta name="url" content={$page.url.href} />
-	<meta name="identifier-URL" content={$page.url.origin} />
+	<!-- <meta name="url" content={$page.url.href} /> -->
+	<!-- <meta name="identifier-URL" content={$page.url.origin} /> -->
 	<meta name="category" content="technology, portfolio, blog, landing page, front page" />
 	<meta name="coverage" content="Worldwide" />
 	<meta name="distribution" content="Global" />
 	<meta name="rating" content="General" />
 
 	<!-- OpenGraph Meta Tags -->
-	<meta name="og:title" content={title} />
+	<meta name="og:title" content="Hi, I'm Elclark!" />
 	<meta name="og:type" content="website" />
-	<meta name="og:url" content={$page.url.href} />
-	<meta name="og:image" content={image} />
+	<!-- <meta name="og:url" content={$page.url.href} /> -->
+	<meta name="og:image" content="https://elclark.my.id/img/brand/elclark.png" />
 	<meta name="og:site_name" content="Elclark" />
-	<meta name="og:description" content={description} />
+	<meta
+		name="og:description"
+		content="I'm a software engineer and system administrator from Manado, Indonesia. I also a university student and UI/UX designer. I'm passionate about technology, science, music, and gaming."
+	/>
 
 	<meta name="og:email" content="mail@elclark.my.id" />
 	<meta name="og:region" content="MDO" />
 	<meta name="og:country-name" content="ID" />
 
 	<!-- Twitter Metadata -->
-	<meta name="twitter:title" content={title} />
-	<meta name="twitter:description" content={description} />
-	<meta name="twitter:url" content={image} />
+	<meta name="twitter:title" content="Hi, I'm Elclark!" />
+	<meta
+		name="twitter:description"
+		content="I'm a software engineer and system administrator from Manado, Indonesia. I also a university student and UI/UX designer. I'm passionate about technology, science, music, and gaming."
+	/>
+	<meta name="twitter:url" content="https://elclark.my.id/img/brand/elclark.png" />
 	<meta name="twitter:card" content="summary" />
 </svelte:head>
 
