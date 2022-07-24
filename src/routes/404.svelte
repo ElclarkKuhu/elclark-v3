5c84a5ed8450a94c66e4be70b8de918bff851a65
Elclark <mail@elclark.my.id>
2022-07-24 14:33:09 +0800

    :sparkles: add 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,23 @@
+<div class="container">
+	<h1>404 - Not Found</h1>
+	<p class="description">The page you are looking for does not exist.</p>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		min-height: 75vh;
+		padding: 1.25rem;
+	}
+
+	h1 {
+		margin: 0;
+	}
+
+	p {
+		margin: 0;
+	}
+</style>
