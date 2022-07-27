c024cc0b9174cae7edaa3751e7d1b675fb472a40
Elclark <mail@elclark.my.id>
2022-07-27 11:15:51 +0800

    :art: page transitions & always show scrollbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,17 @@
 <script>
 	import Header from '$components/header.svelte'
 	import Footer from '$components/footer.svelte'
+
+	import { page } from '$app/stores'
+	import { fade } from 'svelte/transition'
 </script>
 
 <Header />
-<slot />
+{#key $page.url}
+	<div in:fade>
+		<slot />
+	</div>
+{/key}
 <Footer />
 
 <style>
