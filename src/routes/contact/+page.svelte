7032afe57d14e10fb8988c70992ae98b022fe849
Elclark <mail@elclark.my.id>
2022-08-29 09:56:59 +0800

    cleanup

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,7 +4,7 @@
 	import Meta from '$components/meta.svelte'
 	import Button from '$components/button.svelte'
 
-	import Badge from './badge.svelte'
+	import Badge from '$components/badge.svelte'
 </script>
 
 <Meta
@@ -17,8 +17,8 @@
 <div class="container">
 	<main>
 		<Badge />
-
 		<h1>Contact Me</h1>
+
 		<p class="about">
 			If you have any questions or want to get in touch, please feel free to contact me. I will get
 			back to you as soon as possible. Thanks for your interest!
